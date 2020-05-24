dbe064d48405d6d2dfa4ee908cbfa24c2ca408a2
Femi Ayodeji <feminstincts@gmail.com>
2020-05-24 16:41:32 +0100

    added in-memory student repo implementing IStudentRepo interface

---
+++ Data/InMemoryStudentRepo.cs
@@ -0,0 +1,10 @@
+namespace StudentService.Data
+{
+    public class InMemoryStudentRepo : IStudentRepo
+    {
+        public void CreateStudent()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
